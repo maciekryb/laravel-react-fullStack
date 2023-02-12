a70cbb887d06b6e259061a384ad1e0daf0f726d3
Maciej Rybak <maciek.rybak95@gmail.com>
2023-02-12 20:54:31 +0100

    add error information

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import axiosClient from "../axios-client";
 import { Link } from "react-router-dom";
 import { useStateContext } from "../context/ContextProvider";
@@ -8,6 +8,7 @@ const Signup = () => {
   const emailRef = useRef();
   const passowrdRef = useRef();
   const passwordConfirmationRef = useRef();
+  const [errors, setErrors] = useState(null);
 
   const { setUser, setToken } = useStateContext();
 
@@ -29,7 +30,7 @@ const Signup = () => {
       .catch((err) => {
         const response = err.response;
         if (response && response.status === 422) {
-          console.log(response.data.errors);
+          setErrors(response.data.errors);
         }
       });
   };
@@ -39,6 +40,14 @@ const Signup = () => {
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Signup for free</h1>
+
+          {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}{" "}
+            </div>
+          )}
           <input ref={nameRef} placeholder="Full Name" />
           <input ref={emailRef} placeholder="Email Adress" type="email" />
           <input ref={passowrdRef} placeholder="Password" type="password" />
